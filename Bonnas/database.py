dff81fe7622895ad1d16eaddeb8066dec3948a45
Christiaan Holm <u20418249@tuks.co.za>
2024-06-12 13:44:06 +0200

    cleanup

--- Bonnas/database.py
+++
@@ -1,7 +0,0 @@
-#The DAB functionality
-from externals import *
-
-def dabMenu() :
-	printblue("[dbConn] Connect to the database")
-	printblue("[dbCls] Terminate the conncetion to the database")
-	printblue("[dbApp] Atdd user to the database")
